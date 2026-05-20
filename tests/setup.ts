d8633812ad5cb5f3705f00f2637786ca927b5193
benaltr <beb.altr@gmail.com>
2026-05-20 14:07:16 +0300

    chore(test): add setup hooks file for Vitest

---
+++ tests/setup.ts
@@ -0,0 +1,9 @@
+import { afterAll, beforeAll } from "vitest";
+
+beforeAll(() => {
+  // Reserved for global test setup (DB connections, etc.)
+});
+
+afterAll(() => {
+  // Reserved for global teardown
+});
